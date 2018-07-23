9289bada1eb6b5257a5189745eb7fb348c0cbb67
saffahyjp <saffah@126.com>
2018-07-23 14:40:55 +0800

    very simple but working sample engine

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1,20 +1,32 @@
 #include "engine.h"
+#include <cstdlib>
 
 namespace DGEBC
 {
 	typedef Engine::input_t input_t;
 	typedef Engine::output_t output_t;
+	using namespace std;
 	output_t Engine::score(const input_t &in)
 	{
-		return 0;
+		try
+		{
+			output_t x = stold(in);
+			return 1 / (pow(x - 250, 2) + 1);
+		}
+		catch(exception e)
+		{
+			return 0;
+		}
 	}
 	input_t Engine::combine(const input_t &in1, const input_t &in2)
 	{
-		return in1;
+		output_t x = stold(in1), y = stold(in2);
+		return to_string((x + y) / 2);
 	}
 	input_t Engine::mutate(const input_t &in)
 	{
-		return in;
+		output_t x = stold(in);
+		return to_string(x - 10 + rand() * 20. / RAND_MAX);
 	}
 #ifndef DGEBC_ENGINE_NO_VISUALIZE
 	QWidget *visualize(const input_t &in)
