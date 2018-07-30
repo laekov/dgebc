0ea9c028b13130ed3e88583d284051510eda21d8
Rick Ho <laekov.h@gmail.com>
2018-07-30 22:01:12 +0800

    fix batchrun script

--- worker/batchrun.sh
+++ worker/batchrun.sh
@@ -1,13 +1,18 @@
 export LD_LIBRARY_PATH=../engine 
 trap ctrl_c INT
 
-for ((i=0;i<$1;++i)) 
-do
-	bin/main -w 5 -p 90$(printf "%02d" $i) -h e1.sc.team -q 2333 2>&1 | tee $i.log &
-done
-
 function ctrl_c() {
 	killall main;
 	echo 'killed';
 	exit
 }
+
+for ((i=0;i<$1;++i)) 
+do
+	bin/main -w 5 -p 90$(printf "%02d" $i) -h e1.sc.team -q 2333 2>&1 | tee $i.log &
+done
+
+while true
+do
+	false
+done
